a0c80e50ba59a5d1e35b181771b863d1018355a2
Iñigo Montánchez <mci.m89@gmail.com>
2023-08-05 13:27:22 +0200

    feat: searcher tool, change button home from info page and add bibliography, add a first concept of the first gallery and first species card.

---
+++ src/scripts/searcher.js
@@ -0,0 +1,28 @@
+function search() {
+    searcher();
+}
+
+async function searcher() {
+    const input = document.querySelector("#data");
+    if (input) {
+        const data = input.value;
+        const jsonData = await fetchSpeciesFor();
+        console.log(jsonData);
+
+        for (var i = 0; i < jsonData.length; i++) {
+            if (jsonData[i]['common-name'] == data) {
+                window.location.href=jsonData[i]['url'];
+            } else if (jsonData[i]['latin'] == data) {
+                window.location.href=jsonData[i]['url'];
+            } else if (jsonData[i]['id'] == data) {
+                window.location.href=jsonData[i]['url'];
+            }
+        
+        }
+    }
+}
+
+async function fetchSpeciesFor(newLocale) {
+    const response = await fetch(`https://raw.githubusercontent.com/imc89/ABYSSE/main/src/data/data.json`);
+    return await response.json();
+}
