160bd3cbbc2a2ac26215441fe2d3c276cb52bc5c
Iñigo Montánchez <mci.m89@gmail.com>
2023-11-24 22:01:15 +0100

    feat: change gallery screen.

---
+++ src/scripts/galleryLoaders/gallery_1Loader.js
@@ -0,0 +1,28 @@
+function table() {
+    const imageContainer = document.createElement('div');
+    document.body.appendChild(imageContainer);
+  
+  for (var i=1; i<24; i++) {
+      const imageDiv = document.createElement('div');
+      imageContainer.style.display = 'flex';
+  imageContainer.style.flexWrap = 'wrap'; // Wrap images to form rows
+      imageDiv.className = 'glow-img-container'; // Assign the 'glow' class to the div
+      
+      const imageElement = document.createElement('img');
+      
+  
+      imageElement.src = `../../../img/galleries/gallery_1/fishes_${i}.jpg`;
+      imageElement.className = 'glow-img'
+      imageElement.style.margin= "20px"
+      imageElement.style.width = '150px'
+      imageElement.classList.add = "glow"
+      const imageURL = `./fishes/fishes_${i}.html`;
+  
+    imageElement.addEventListener('click', () => {
+      window.open(imageURL, '_self');
+    });
+  // imageContainer.appendChild(imageElement);
+      imageDiv.appendChild(imageElement); // Append the image to its respective div
+    imageContainer.appendChild(imageDiv);  
+  }
+  }
