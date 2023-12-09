d46ef29be04c6c0dae09ee74075383040cf66fe7
Iñigo Montánchez <mci.m89@gmail.com>
2023-12-09 21:30:55 +0100

    feat: add info new species.

--- src/scripts/infoModal.js
+++ src/scripts/infoModal.js
@@ -1,41 +1,45 @@
 async function jsonInfo(info_json) {
-    const response = await fetch(info_json);
-    const jsonData = await response.json();
-    openModal(jsonData);
+  const response = await fetch(info_json);
+  const jsonData = await response.json();
+  openModal(jsonData);
 }
 
 function openModal(info_json) {
-// Create the modal
-const modalContainer = document.getElementById('modal-container');
-const modalContent = document.getElementById('modal-content');
+  // Disable scrolling for the main page
+  document.body.style.overflow = 'hidden';
 
-// Add class to make modal fullscreen
-modalContainer.classList.add('modal-fullscreen');
-modalContent.classList.add('modal-fullscreen');
+  // Create the modal
+  const modalContainer = document.getElementById('modal-container');
+  const modalContent = document.getElementById('modal-content');
 
-// Set modal content to display lorem ipsum for the selected name
-modalContent.innerHTML = `
-<div style="text-align: center">
-<h2 style="color: white; font-style: bold; text-align: center;">${info_json.name.toUpperCase()}</h2>
-<img src="${info_json.img_1}" class="glow-img card-img" style="display: block; margin: 0 auto; margin-top: 30px; width="325"/>
-    <p style="color: white; text-align: center;">${info_json.info_1}</p>
-    <hr>
-    <p style="color: white; text-align: center;">${info_json.info_2}</p>
-    <hr>
-    <p style="color: white; text-align: center;">${info_json.info_3}</p>
-</div>
-`;
-// Set modal content to display image
-// modalContent.innerHTML = `<img src="${info_json.img_1}" class="glow-img card-img" style="display: block; margin: 0 auto; margin-top: 30px; width="325" />`;
+  // Add class to make modal fullscreen
+  modalContainer.classList.add('modal-fullscreen');
+  modalContent.classList.add('modal-fullscreen');
 
-// Show the modal
-modalContainer.style.display = 'block';
-window.scrollTo(0, 0);
-// Close the modal on click outside
-modalContainer.addEventListener('click', (event) => {
-    
-  if (!event.target.closest('.modal-content')) {
+  // Set modal content to display lorem ipsum for the selected name
+  modalContent.innerHTML = `
+    <div style="text-align: center; position: absolute; top: 0; left: 0; width: 100%; height: 100%; overflow: auto;">
+      <div class="modal-content-inner" style="height: 100%; overflow-y: auto;">
+        <h2 style="color: white; font-style: bold; text-align: center;">${info_json.name.toUpperCase()}</h2>
+        <img src="${info_json.img_1}" class="glow-img card-img" style="display: block; margin: 0 auto; margin-top: 30px; width="325"/>
+        <p style="color: white; text-align: center;">${info_json.info_1}</p>
+        <hr>
+        <p style="color: white; text-align: center;">${info_json.info_2}</p>
+        <hr>
+        <p style="color: white; text-align: center;">${info_json.info_3}</p>
+      </div>
+    </div>
+  `;
+
+  // Show the modal
+  modalContainer.style.display = 'block';
+
+  // Enable scrolling only for the modal container
+  modalContainer.style.overflow = 'auto';
+
+  // Close the modal on click outside
+  modalContainer.addEventListener('click', (event) => {
+    document.body.style.overflow = ''; // Re-enable scrolling for the main page
     modalContainer.style.display = 'none';
-  }
-});
+  });
 }
